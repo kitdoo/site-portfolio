b78f2ac618b26c512d25316c88b28b88cb0759d8
Kitdoo <ira.bondarevskaya@gmail.com>
2019-02-27 14:19:19 +0700

    header

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-	/* Call plugins hrere ...*/
+	//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a,a[href='#top'],a[href='#resume'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
 
 	
 });
