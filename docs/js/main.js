b4cce1eae06164e6517a3c28725952d6bf50a7da
Kitdoo <ira.bondarevskaya@gmail.com>
2019-02-27 14:27:54 +0700

    final portfolio study stage

--- docs/js/main.js
+++ docs/js/main.js
@@ -19,6 +19,14 @@ $(document).ready(function() {
     });
 	// End of FancyBox - galery
 
+	var filter_ramka = $('.filter-block__button--active');
+    $('.filter-block__button').on('click', function(e) {        
+        if (filter_ramka) filter_ramka.removeClass('filter-block__button--active');
+        $(this).addClass('filter-block__button--active');
+        filter_ramka = $(this);
+    });
+
+	
 
 	// jQuery Validate JS
 	$("#contact-form").validate({
@@ -37,8 +45,27 @@ $(document).ready(function() {
 				email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой."
 			},
 			message: "Пожалуйста, введите текст сообщения"
-		}
-	})
-
+		},
+		    submitHandler: function(form) {
+            ajaxFormSubmit();
+        }
 	
-});
+  })
+
+	    function ajaxFormSubmit() {
+        var string = $("#contact-form").serialize();
+        
+        $.ajax( {
+            type: "POST",
+            url: "php/mail.php",
+            data: string,
+            success: function(html) {
+                $("#contact-form").slideUp(800);
+                $('#answer').html(html);
+            }
+        });
+        
+        return false;
+    }
+
+    }); 
