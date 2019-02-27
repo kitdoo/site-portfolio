ef846573f51e67cfd4b81af407b4a1bf388d8644
Kitdoo <ira.bondarevskaya@gmail.com>
2019-02-27 14:22:20 +0700

    filter

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 	$("nav a,a[href='#top'],a[href='#resume'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
 	    highlightSelector:"nav a"
 	});
+	
+	// MixItUp - фильтрация работ в портфолио
+	$('#portfolio-projects').mixItUp();
 
 	
 });
